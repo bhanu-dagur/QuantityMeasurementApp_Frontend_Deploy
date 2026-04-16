6e07e84911b3a537ca298963d91315f83da03411
bhanu-dagur <bhanudagur1702@gmail.com>
2026-04-16 13:07:50 +0530

    d

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://quantitymeasurementapp-backend-deploy.onrender.com'
+  apiUrl: 'https://quantitymeasurementapp-backend-deploy-1.onrender.com'
 };
