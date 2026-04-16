1face8eb432c34d7542a72a9f4d503c60ae68746
bhanu-dagur <bhanudagur1702@gmail.com>
2026-04-16 14:57:37 +0530

    A

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://quantitymeasurementapp-backend-deploy-1.onrender.com/api'
+  apiUrl: 'https://quantitymeasurementapp-backend-deploy-1.onrender.com/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://quantitymeasurementapp-backend-deploy-1.onrender.com'
+  apiUrl: 'https://quantitymeasurementapp-backend-deploy-1.onrender.com/api/'
 };
